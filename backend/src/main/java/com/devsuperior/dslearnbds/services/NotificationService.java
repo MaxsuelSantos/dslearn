8c026ea0d526a4b0832dd138d65a920d79feb854
Maxsuel Santos <santosmaxsuel17@gmail.com>
2021-07-02 12:02:18 -0300

    Notification unreadOnly

--- backend/src/main/java/com/devsuperior/dslearnbds/services/NotificationService.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/services/NotificationService.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Service
 public class NotificationService {
@@ -20,9 +21,10 @@ public class NotificationService {
     private AuthService authService;
 
     @Transactional(readOnly = true)
-    public Page<NotificationDTO> notificationsForCurrentUser(Pageable pageable) {
+    public Page<NotificationDTO> notificationsForCurrentUser(boolean unreadOnly, Pageable pageable) {
+
         User user = authService.authenticated();
-        Page<Notification> page = repository.findByUser(user, pageable);
+        Page<Notification> page = repository.find(user, unreadOnly, pageable);
         return page.map(NotificationDTO::new);
     }
 
